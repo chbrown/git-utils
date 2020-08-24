9358795bf3c7b3338dc8274cb1d028da5c0d2789
Christopher Brown <io@henrian.com>
2020-08-24 12:18:21 -0400

    Add 'normalize_url' helper

--- git_utils/util.py
+++ git_utils/util.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Any, Callable, Iterator, Union
 import json
 import os
+import re
 
 from requests.models import CaseInsensitiveDict
 
@@ -40,3 +41,13 @@ def sumsize(top: Union[str, Path]) -> int:
     """
     top = Path(top)
     return sum(path.stat().st_size for path in walk(top))
+
+
+def normalize_url(url: str) -> str:
+    """
+    Fix git URLs with "alternative scp-like syntax", returning other URLs unchanged.
+    """
+    if m := re.match(r"^([^:/?#]+@)?([^:/?#]+):([^/].+)$", url):
+        #               ↑ 1=user    ↑ 2=host   ↑ 3=path + query + fragment
+        return f"ssh://{m.group(1)}{m.group(2)}/{m.group(3)}"
+    return url
