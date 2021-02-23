984752fb732f1623cb3e60051110c782a9afed2b
Christopher Brown <io@henrian.com>
2021-02-23 17:14:38 -0500

    Move 'request_and_print_first_and_last_pages' from 'Client' to 'API'

--- git_utils/github/client.py
+++ git_utils/github/client.py
@@ -6,8 +6,6 @@ import urllib
 from requests import Session, Response
 from requests.utils import parse_header_links
 
-from . import print_response
-
 logger = logging.getLogger(__name__)
 
 
@@ -76,17 +74,3 @@ class Client:
         """
         for response in self.iter_requests(url, **kwargs):
             yield from response.json()
-
-    def request_and_print_first_and_last_pages(self, url: str, **kwargs):
-        kwargs.setdefault("params", {}).setdefault("per_page", 100)
-        response = self.request(url, **kwargs)
-        print_response(response)
-        links = {
-            link["rel"]: link["url"]
-            for link in parse_header_links(response.headers.get("Link", ""))
-        }
-        # jump to last page if there are multiple pages
-        if "last" in links:
-            logger.info("...")
-            response = self.request(links["last"], **kwargs)
-            print_response(response)
